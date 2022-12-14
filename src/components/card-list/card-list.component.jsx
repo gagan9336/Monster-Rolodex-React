a337440d25d23a183351f7f8693064ee0f2eea1b
unknown <deepgagan9336@gmail.com>
2022-12-14 13:33:58 +0530

    first commit

---
+++ src/components/card-list/card-list.component.jsx
@@ -0,0 +1,22 @@
+import { Component } from 'react';
+import './card-list.styles.css';
+import Card from '../card/card.component.jsx';
+
+class CardList extends Component {
+    render () {
+        const { monsters } = this.props;  
+        return (
+            <div className='card-list'>
+                
+                {
+                    monsters.map((monster) => {
+                       return <Card monsterHandler = {monster} />
+                    })
+                }
+            </div>
+            
+        )
+    }
+}
+
+export default CardList;
